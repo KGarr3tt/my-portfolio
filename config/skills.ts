5a34c344dfda677146a6cfeffecc75fcf1764617
Karl Garrett <garrettkarl@gmail.com>
2025-10-22 12:22:48 +0100

    Updates skills to be more relevant. Clearing space for new additions.

--- config/skills.ts
+++ config/skills.ts
@@ -8,20 +8,20 @@ export interface skillsInterface {
 }
 
 export const skillsUnsorted: skillsInterface[] = [
-  {
-    name: "Next.js",
-    description:
-      "Effortlessly build dynamic apps with routing, layouts, loading UI, and API routes.",
-    rating: 5,
-    icon: Icons.nextjs,
-  },
-  {
-    name: "React",
-    description:
-      "Craft interactive user interfaces using components, state, props, and virtual DOM.",
-    rating: 5,
-    icon: Icons.react,
-  },
+  // {
+  //   name: "Next.js",
+  //   description:
+  //     "Effortlessly build dynamic apps with routing, layouts, loading UI, and API routes.",
+  //   rating: 5,
+  //   icon: Icons.nextjs,
+  // },
+  // {
+  //   name: "React",
+  //   description:
+  //     "Craft interactive user interfaces using components, state, props, and virtual DOM.",
+  //   rating: 5,
+  //   icon: Icons.react,
+  // },
   {
     name: "GraphQL",
     description:
@@ -71,63 +71,63 @@ export const skillsUnsorted: skillsInterface[] = [
     rating: 5,
     icon: Icons.javascript,
   },
-  {
-    name: "HTML 5",
-    description:
-      "Structure web content beautifully with the latest version of HyperText Markup Language.",
-    rating: 4,
-    icon: Icons.html5,
-  },
-  {
-    name: "CSS 3",
-    description:
-      "Style web pages creatively with the latest iteration of Cascading Style Sheets.",
-    rating: 4,
-    icon: Icons.css3,
-  },
-  {
-    name: "React Native",
-    description:
-      "Develop cross-platform mobile apps using React for consistent and engaging experiences.",
-    rating: 4,
-    icon: Icons.react,
-  },
-  {
-    name: "Angular",
-    description:
-      "Build dynamic web apps with a TypeScript-based open-source framework by Google.",
-    rating: 3,
-    icon: Icons.angular,
-  },
-  {
-    name: "Redux",
-    description:
-      "Manage app state effectively using a predictable and centralized state container.",
-    rating: 4,
-    icon: Icons.redux,
-  },
-  {
-    name: "Socket.io",
-    description:
-      "Enable real-time, bidirectional communication between clients and servers effortlessly.",
-    rating: 3,
-    icon: Icons.socketio,
-  },
-  {
-    name: "Material UI",
-    description:
-      "Create stunning and responsive UIs with a popular React UI framework.",
-    rating: 4,
-    icon: Icons.mui,
-  },
+  // {
+  //   name: "HTML 5",
+  //   description:
+  //     "Structure web content beautifully with the latest version of HyperText Markup Language.",
+  //   rating: 4,
+  //   icon: Icons.html5,
+  // },
+  // {
+  //   name: "CSS 3",
+  //   description:
+  //     "Style web pages creatively with the latest iteration of Cascading Style Sheets.",
+  //   rating: 4,
+  //   icon: Icons.css3,
+  // },
+  // {
+  //   name: "React Native",
+  //   description:
+  //     "Develop cross-platform mobile apps using React for consistent and engaging experiences.",
+  //   rating: 4,
+  //   icon: Icons.react,
+  // },
+  // {
+  //   name: "Angular",
+  //   description:
+  //     "Build dynamic web apps with a TypeScript-based open-source framework by Google.",
+  //   rating: 3,
+  //   icon: Icons.angular,
+  // },
+  // {
+  //   name: "Redux",
+  //   description:
+  //     "Manage app state effectively using a predictable and centralized state container.",
+  //   rating: 4,
+  //   icon: Icons.redux,
+  // },
+  // {
+  //   name: "Socket.io",
+  //   description:
+  //     "Enable real-time, bidirectional communication between clients and servers effortlessly.",
+  //   rating: 3,
+  //   icon: Icons.socketio,
+  // },
+  // {
+  //   name: "Material UI",
+  //   description:
+  //     "Create stunning and responsive UIs with a popular React UI framework.",
+  //   rating: 4,
+  //   icon: Icons.mui,
+  // },
 
-  {
-    name: "Tailwind CSS",
-    description:
-      "Design beautiful, modern websites faster with a utility-first CSS framework.",
-    rating: 5,
-    icon: Icons.tailwindcss,
-  },
+  // {
+  //   name: "Tailwind CSS",
+  //   description:
+  //     "Design beautiful, modern websites faster with a utility-first CSS framework.",
+  //   rating: 5,
+  //   icon: Icons.tailwindcss,
+  // },
   {
     name: "AWS",
     description:
@@ -135,20 +135,20 @@ export const skillsUnsorted: skillsInterface[] = [
     rating: 3,
     icon: Icons.amazonaws,
   },
-  {
-    name: "Bootstrap",
-    description:
-      "Quickly create responsive and appealing web designs using a popular CSS framework.",
-    rating: 2,
-    icon: Icons.bootstrap,
-  },
-  {
-    name: "MySQL",
-    description:
-      "Manage and organize relational databases efficiently for data-driven applications.",
-    rating: 2,
-    icon: Icons.mysql,
-  },
+  // {
+  //   name: "Bootstrap",
+  //   description:
+  //     "Quickly create responsive and appealing web designs using a popular CSS framework.",
+  //   rating: 2,
+  //   icon: Icons.bootstrap,
+  // },
+  // {
+  //   name: "MySQL",
+  //   description:
+  //     "Manage and organize relational databases efficiently for data-driven applications.",
+  //   rating: 2,
+  //   icon: Icons.mysql,
+  // },
   {
     name: "Netlify",
     description:
